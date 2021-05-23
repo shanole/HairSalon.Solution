9d26a4441bfe53db9d9c6db95dc521e72149bb70
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 19:35:29 -0700

    fix typos

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,10 +4,10 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    public virtual DbSet<Category> Stylists { get; set; }
-    public DbSet<Item> Clients { get; set; }
+    public virtual DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
-    public ToDoListContext(DbContextOptions options) : base(options) { }
+    public HairSalonContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
