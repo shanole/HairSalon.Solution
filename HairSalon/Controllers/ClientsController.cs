fb150a3e4fedfa786723e9ab069586a50147c29d
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 21:22:55 -0700

    add viewbag to Edit route to show Stylist dropdown

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -39,6 +39,7 @@ namespace HairSalon.Controllers
     public ActionResult Edit(int id)
     {
       Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(thisClient);
     }
     [HttpPost]
