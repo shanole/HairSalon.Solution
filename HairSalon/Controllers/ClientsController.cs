f94291175bca51e412890680263ab631678f563c
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 21:02:21 -0700

    add ViewBag to Create view to show dropdown for Stylists

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,6 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,7 @@ namespace HairSalon.Controllers
     }
     public ActionResult Create()
     {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
     [HttpPost]
