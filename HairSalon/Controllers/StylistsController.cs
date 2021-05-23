2faeea408db68a6a5a762db919971ca70020cb6d
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 19:48:37 -0700

    create routes to get Index and Create views

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,26 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
