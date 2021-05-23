ad5b08649ed4c81bae35484cf8d1d55494ac1ed4
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 20:57:55 -0700

    fix brackets

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -6,55 +6,58 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  private readonly HairSalonContext _db;
-  public ClientsController(HairSalonContext db)
+  public class ClientsController : Controller
   {
-    _db = db;
-  }
-  public ActionResult Index()
-  {
-    List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
-    return View(model);
-  }
-  public ActionResult Create()
-  {
-    return View();
-  }
-  [HttpPost]
-  public ActionResult Create(Client client)
-  {
-    _db.Clients.Add(client);
-    _db.SaveChanges();
-    return RedirectToAction("Index");
-  }
-  public ActionResult Details(int id)
-  {
-    Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-    return View(thisClient);
-  }
-  public ActionResult Edit(int id)
-  {
-    Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-    return View(thisClient);
-  }
-  [HttpPost]
-  public ActionResult Edit(Client client)
-  {
-    _db.Entry(client).State = EntityState.Modified;
-    _db.SaveChanges();
-    return RedirectToAction("Index");
-  }
-  public ActionResult Delete(int id)
-  {
-    Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-    return View(thisClient);
-  }
-  [HttpPost, ActionName("Delete")]
-  public ActionResult DeleteConfirmed(int id)
-  {
-    Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-    _db.Clients.Remove(thisClient);
-    _db.SaveChanges();
-    return RedirectToAction("Index");
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
