a667992e66780f3a14c159648665a8f9cf268d49
shanole <73411129+shanole@users.noreply.github.com>
2021-05-22 21:38:19 -0700

    fix spacing

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -11,6 +11,6 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Details { get; set; }
-     public virtual ICollection<Client> Clients { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
